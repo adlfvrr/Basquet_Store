a32a3a886f7e7dd4c8fc7ac8fde04e46c4dc0074
adlfvrr <adolfover4@gmail.com>
2026-05-09 08:50:54 -0300

    Initial commit

---
+++ basquet-store-api/src/main/java/com/basquetstore/basquet_store_api/BasquetStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.basquetstore.basquet_store_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasquetStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasquetStoreApiApplication.class, args);
+	}
+
+}
---
+++ basquet-store-api/src/test/java/com/basquetstore/basquet_store_api/BasquetStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basquetstore.basquet_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasquetStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
